892636c6c6c2190603edc3d91cb2da95252b1980
Quyen Thien <phamquyenthien@gmail.com>
2022-10-29 21:35:00 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
+import 'package:myshop/ui/screens.dart';
 import 'package:provider/provider.dart';
 
 import 'products_grid.dart';
@@ -17,21 +18,41 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-  var _showOnlyFavorites = false;
+  final _showOnlyFavorites = ValueNotifier<bool>(false);
+  late Future<void> _fetchProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProducts = context.read<ProductsManager>().fetchProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShop'),
-        actions: <Widget>[
-          buildProductFilterMenu(),
-          buildShoppingCartIcon(),
-        ],
-      ),
-      drawer: const AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
-    );
+        appBar: AppBar(
+          title: const Text('MyShop'),
+          actions: <Widget>[
+            buildProductFilterMenu(),
+            buildShoppingCartIcon(),
+          ],
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future: _fetchProducts,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ValueListenableBuilder<bool>(
+                  valueListenable: _showOnlyFavorites,
+                  builder: (context, onlyFavorites, child) {
+                    return ProductsGrid(onlyFavorites);
+                  });
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
   }
 
   Widget buildShoppingCartIcon() {
@@ -55,13 +76,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   Widget buildProductFilterMenu() {
     return PopupMenuButton(
       onSelected: (FilterOptions selectedValue) {
-        setState(() {
-          if (selectedValue == FilterOptions.favorite) {
-            _showOnlyFavorites = true;
-          } else {
-            _showOnlyFavorites = false;
-          }
-        });
+        if (selectedValue == FilterOptions.favorite) {
+          _showOnlyFavorites.value = true;
+        } else {
+          _showOnlyFavorites.value = false;
+        }
       },
       icon: const Icon(
         Icons.more_vert,
--- lib/ui/products/user_products_screen.dart
+++ lib/ui/products/user_products_screen.dart
@@ -10,6 +10,10 @@ class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
   const UserProductsScreen({super.key});
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await context.read<ProductsManager>().fetchProducts(true);
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsManager = ProductsManager();
@@ -21,9 +25,19 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () async => print('refresh products'),
-        child: buildUserProductListView(productsManager),
+      body: FutureBuilder(
+        future: _refreshProducts(context),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return RefreshIndicator(
+            onRefresh: () => _refreshProducts(context),
+            child: buildUserProductListView(productsManager),
+          );
+        },
       ),
     );
   }
@@ -57,64 +71,3 @@ class UserProductsScreen extends StatelessWidget {
     );
   }
 }
- 
-/* 
-import 'package:flutter/material.dart';
-import 'user_product_list_tile.dart';
-import 'products_manager.dart';
-import '../shared/app_drawer.dart';
-import 'package:provider/provider.dart';
-import 'edit_product_screen.dart';
-
-class UserProductsScreen extends StatelessWidget {
-  static const routeName = '/user-products';
-  const UserProductsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final productsManager = ProductsManager();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Products'),
-        actions: <Widget>[
-          buildAddButton(context),
-        ],
-      ),
-      drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () async => print('refresh products'),
-        child: buildUserProductListView(productsManager),
-      ),
-    );
-  }
-
-  Widget buildUserProductListView(ProductsManager productsManager) {
-    return Consumer<ProductsManager>(
-      builder: (ctx, productsMaager, child) {
-        return ListView.builder(
-          itemCount: productsManager.itemCount,
-          itemBuilder: (ctx, i) => Column(
-            children: [
-              UserProductListTile(
-                productsManager.items[i],
-              ),
-              const Divider(),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget buildAddButton(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.add),
-      onPressed: () {
-        Navigator.of(context).pushNamed(
-          EditProductScreen.routeName,
-        );
-      },
-    );
-  }
-}
- */
